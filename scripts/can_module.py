bf88bdd610002ed2261e196478d24fcd0cdeb486
gladlab225 <gladlab225@gmail.com>
2023-06-08 16:53:46 +0900

    Set Copyright

--- scripts/can_module.py
+++ scripts/can_module.py
@@ -1,3 +1,33 @@
+# Copyright (c) 2023, Gwangju Institute of Science and Technology
+# All rights reserved.
+
+# This code was developed by Hong-Seung Kim, Ji-Kang Kong, and Taehyung Gil as part of their
+# work as interns and graduate students at the GLAD lab, Gwangju Institute of Science and
+# Technology, Gwangju, South Korea.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this code
+# and associated documentation files (the "Code"), to use the Code for academic purposes only,
+# subject to the following conditions:
+
+# The Code may only be used for academic, non-commercial purposes.
+# Any commercial use of the Code or any part thereof is strictly prohibited, and requires
+# a separate, written license from the GLAD lab.
+# Redistributions of source code must retain the above copyright notice, this list of
+# conditions, and the following disclaimer.
+
+# For any commercial use or licensing inquiries, please contact:
+# Professor Yong-Gu Lee
+# GLAD Lab, Gwangju Institute of Science and Technology
+# Address: Mechanical Engineering #210, 123 Cheomdangwagi-ro,  Buk-gu, Gwangju, 61005, Korea
+# Email: lygu@gist.ac.kr
+
+# THE CODE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+
+
 import cantools
 import can
 import threading
